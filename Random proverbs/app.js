2aadcb16ca89faf9853d9bcb3df02619ae7eddd2
Courage Ujiagbe <105734915+CourageEkata@users.noreply.github.com>
2022-08-30 01:12:30 +0100

    Add files via upload

---
+++ Random proverbs/app.js
@@ -0,0 +1,477 @@
+const proverbs = [
+  {
+    title: "Arrow of God",
+    proverb:
+      " When an adult is in the house, the she-goat is not left to suffer the pains of parturition on its tether.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The world is like a Mask dancing. If you want to see it well, you do not stand in one place.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "It is praiseworthy to be brave and fearless but sometimes, it is better to be a coward. ",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      " We often stand in the compound of a coward to point at the ruins where a brave man used to live.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The man who has never submitted to anything will soon submit to the burial mat.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: " A man does not go to his in-law with wisdom.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When a handshake goes beyond the elbow we know it has turned to another thing.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "Wisdom is like a goatskin bag; every man carries his own.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      " If the lizard of the homestead should neglect to do the things for which its kind is known, it will be mistaken for the lizard of the farmland.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "Do you blame a vulture for perching over a carcass?",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "When a man of cunning dies, a man of cunning buries him.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "A toad does not run in the day unless something is after it",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The fly that has no one to advise it follows the corpse into the grave.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "Let the slave who sees another cast into a shallow grave know that he will be buried in the same way when his day comes.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When we see a little bird dancing in the middle of the pathway, we must know that its drummer is in the near-by bush.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "If a man says yes, his chi also says yes.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "The inquisitive monkey gets a bullet in the face",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "Whatever music you beat on your drum there is somebody who can dance to it.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A coward may cover the ground with its words but when the time comes to fight he runs away.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "Unless the wind blows, we do not see the fowl's rump.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A man who brings home ant-infested faggots should not complain if he is visited by lizards.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The very thing which kills mother rat prevents the little ones from opening their eyes.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "An old woman is never old when it comes to the dance she knows.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When suffering knocks at your door and you say there is no seat left for him, he tells you not to worry because he has brought his own stool. ",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A hot soup must be taken slowly-slowly from the edges of the bowl.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "The death that will kill a man begins as an appetite.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: " To say My father told me is to swear the greatest oath.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "One should not expect to be provided the snuff and also the walking around, to be given a wife and a mat to sleep on.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A man can swear before the most dreaded deity on what his father told him.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A man cannot be too busy to break the first kolanut of the day in his own house.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "A man who visits a craftsman at work finds a sullen host.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The lizard who fell down from the high Iroko tree felt entitled to praise himself if nobody else did.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The lizard who threw confusion into his mother's funeral rite, did he expect outsiders to carry the burden of honouring his dead",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "The offspring of a hawk cannot fail to devour chicks.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The fly that perches on a mound of dung may strut around as it likes, it cannot move the mound.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "When two brothers fight, a stranger reaps the harvest.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The man who brings ant-infested faggots into his hut should not grumble when lizards begin to pay him a visit.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A disease that has never been seen before cannot be cured with everyday herbs.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When we want to make a charm, we look for the animal whose blood can match its power, if a chicken cannot do it we look for a goat or a ram; if that is sufficient we send for a bull. ",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "No matter how many spirits plotted a man's death, it would come to nothing unless his personal god took a hand on the deliberation.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A man who has nowhere else to put his hand for support puts it on his own knees.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A snake is never as long as the stick to which we liken its strength",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "Unless the penis dies young, it will surely eat bearded meat.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "As soon as we shake hands with a leper, he will want an embrace.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "It is not our custom to show our neighbour's creditors the way to his hut.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When a masked spirit visits you, you have to appease its footprints with presents.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "A fowl does not eat into the belly of a goat.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "Until a man wrestles with one of those who make a path across his homestead, the others will not stop. ",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "A hostile clansman is a friend in a strange country.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "It is the fear of causing offence that makes men swallow poison.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A woman cannot place more than the length of her leg on her husband.",
+  },
+  {
+    title: "Arrow of God",
+  proverb:
+      "The young he-goat said that but for his sojourn in his mother's clan, he would not have learnt to stick up his upper lip. ",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "If the rat could not run fast enough it must make way for the tortoise.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "Every lizard lies on its belly, so we cannot tell which has a bellyache.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A woman who carries her head on a rigid neck as if she is carrying a pot of water will never live for long with any husband.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When mother-cow is cropping giant grass her calves watch her mouth.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "It might be thought foolish for a man to spit out a morsel which fortune had placed in his mouth but in certain circumstances such a man compelled respect.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The man to fear in action is the one who first submits to suffer to the limit.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The toad lost the chance of growing a tail because of I am coming, I am coming.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "A man must dance the dance prevalent in his time.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The noise even of the loudest events must begin to die down by the second market week.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "A man who asks questions does not lose his way.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The person who sets a child to catch a shrew should also find him water to wash the odour from his hand.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "The gods sometimes use us as a whip",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "He whose name is called again and again by those trying in vain to catch a wild bull has something he alone can do to bulls.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When the air is fouled by a man on top of a palm tree the fly is confused.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "He who will swallow udala seeds must consider the size of his anus.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The fly that has no one to advise him follows the corpse into the ground.",
+  },
+  {
+    title: "Arrow of God",
+    proverb: "When a handshake passes the elbow, it becomes another thing.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "The little bird which hops off the ground and lands on an ant-hill may not know it but is still on the ground.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A common snake which a man sees all alone may become a python in his eyes",
+  },
+  {
+    title: "Arrow of God",
+  proverb:
+      "The boy who persists in asking what happened to his father before he has enough strength to avenge him is asking for his father's fate. ",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When death wants to take a little dog, it prevents it from smelling even excrement.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "A man is like a funeral ram which must take whatever beating comes to it without opening its mouth.",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "When was it ever heard that a child was scalded by the piece of yam its own mother put in its palm?",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "What man would send his son with a potsherd to bring fire from a neighbour's hut and then unleash rain on him?",
+  },
+  {
+    title: "Arrow of God",
+    proverb:
+      "No man however great was greater than his people; no one ever won judgment against his clan.",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb:
+      "A man who calls his kinsmen to a feast does not do so to save them from starving. They all have food in their own homes. When we gather together in the moonlit village ground it is not because of the moon. Every man can see it in his own compound. We come together because it is good for kinsmen to do so.",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb: "When the moon is shining the cripple becomes hungry for a walk",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb:
+      "Among the Ibo the art of conversation is regarded very highly, and proverbs are the palm-oil with which words are eaten.",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb:
+      "As the elders said, if a child washed his hands he could eat with kings.",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb: " A child cannot pay for its mother's milk.",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb:
+      "Eneke the bird says that since men have learned to shoot without missing, he has learned to fly without perching.",
+  },
+  {
+    title: "Things Fall Apart",
+    proverb:
+      "You do not know me,' said Tortoise. 'I am a changed man. I have learned that a man who makes trouble for others makes trouble for himself.",
+  },
+];
+
+const funFacts = [
+  {
+  fact: "Chinua Achebe was born on the 16th November, 1930. He was a Nigerian novelist, poet, and critic who is regarded as the dominant figure of modern African literature"},
+  {fact: "Achebe's status as the 'father of African literature' is no joke, and it's largely due to his debut novel, Things Fall Apart. Published in 1958."},
+  {fact: "Things Fall Apart which follows the life of Okonkwo, an Igbo leader and wrestling champion—has gone on to sell more than 10 million copies and has been translated into 50 different languages. Even today, nearly 60 years after its original publication, it remains one of the most taught and dissected novels about Africa."},
+  {fact: "There is no place in the world where African Literaature is taught without extensive reference to at least on of Achebe's works."},
+  {fact:"Things Fall Apart belongs to a trilogy which includes Arrow of God and No Longer at Ease."},
+  {fact: "The style of Achebe's fiction draws heavily on the oral tradition of the Igbo people. He incorporates folk tales into his stories, exposing community values in both the content and the form of the storytelling"}
+]
+
+
+const content = document.querySelector(".proverb-content");
+const book = document.querySelector(".book-title");
+const funFact = document.querySelector(".fun-facts")
+
+const randomBtn = document.querySelector(".btn");
+const surpriseBtn = document.querySelector(".surprise-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+
+
+let currFact = 0;
+let currProverb = 0;
+
+
+const showProverb = (words) => {
+  const arrItem = proverbs[words];
+  content.textContent = arrItem.proverb;
+  book.textContent = arrItem.title;
+};
+
+const showFact = (words) => {
+  const facts = funFacts[words];
+  funFact.textContent = facts.fact
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  showProverb(currProverb);
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+  showFact(currFact)
+})
+
+
+randomBtn.addEventListener("click", () => {
+currFact++;
+if (currFact > funFacts.length - 1) {
+  currFact = 0
+}
+  showFact(currFact)
+})
+
+
+nextBtn.addEventListener("click", () => {
+  currProverb++;
+  if (currProverb > proverbs.length - 1) {
+    currProverb = 0;
+  }
+  showProverb(currProverb);
+});
+prevBtn.addEventListener("click", () => {
+  currProverb--;
+  if (currProverb < 0) {
+    currProverb = proverbs.length - 1;
+  }
+  showProverb(currProverb);
+});
+
+surpriseBtn.addEventListener("click", () => {
+currProverb = Math.floor(Math.random() * proverbs.length);
+showProverb(currProverb);
+});
+
+
+const width = window.innerWidth;
+console.log(width);
